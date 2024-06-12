cf9fe3029fd86e919ea7c17f9e2ef39976dd0dab
_.fmendes._ <fmendes_dev@protonmail.ch>
2024-06-12 18:02:32 +0100

    added jwt and unix session timestamp util functions

--- src/common.py
+++ src/common.py
@@ -1,14 +1,21 @@
 from prisma import Prisma
-from sanic import Request, Sanic, Config, json
+from sanic import Request, json
+from typing import Any, Optional, TypedDict, cast
+import datetime
+import jwt
+import time
 
+# Database Client
 db = Prisma()
 
+# API Response Struct
 def formatted_reply(error_message: str | None = None, datum = None):
     return {
         "error_message": error_message,
         "result": datum
     }
 
+# API Error Handler
 async def error_handler(request: Request, exception: Exception):
     message = getattr(exception, "message", "Something Went Wrong")
     status_code = getattr(exception, "status_code", 500)
@@ -16,3 +23,30 @@ async def error_handler(request: Request, exception: Exception):
         formatted_reply(error_message=f"{message if message else exception}"),
         status_code
     )
+
+class JwtPayload(TypedDict):
+    user_id: str
+    exp: Optional[datetime.datetime]
+
+# Sign Json Web Token
+def sign_jwt(payload: JwtPayload) -> str:
+    payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    return jwt.encode(payload=cast(dict[str, Any], payload), key="SECRET", algorithm="HS256")
+
+# Decode Json Web Token
+def decode_jwt(token: str) -> JwtPayload | None:
+    try:
+        return jwt.decode(jwt=token, key="SECRET", algorithms=["HS256"])
+    except:
+        return None
+
+# Generate a session in Unix
+def create_session() -> float:
+    current_time = time.time()
+    three_days_later = current_time + (3 * 24 * 60 * 60)
+    return three_days_later
+
+# Check if the session expired
+def verify_session(session: float) -> bool:
+    current_time = time.time()
+    return current_time > session
